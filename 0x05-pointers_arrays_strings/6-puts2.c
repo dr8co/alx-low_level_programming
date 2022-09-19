07a201c0a0e4ee0c910e56420bdd4ce01bd1002e
Ian Duncan <ianduncanodhiambo@gmail.com>
2022-09-19 11:44:11 -0700

    Corrected 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,13 @@
 void puts2(char *str)
 {
 	int i = 0;
-
-	while (*(str + i))
+	while str[i] != '\0'
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		while (*(str + i))
+		{
+			_putchar(*(str + i));
+			i = i + 2;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
