9d28579b07110756240f3c3a0c1d1e02c15cf348
Ian Duncan <ianduncanodhiambo@gmail.com>
2022-10-07 00:13:06 +0300

    Fixed Betty coding style issues

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,10 +3,10 @@
 
 /**
  * array_range - creates an array of integers.
- * @min: first integer
- * @max: last integer
+ * @min: first integer number
+ * @max: last integer number
  *
- * Return: pointer to newly allocated memory (SUCCESS) or NULL (FAIL)
+ * Return: pointer to newly allocated memory or NULL if fails
  */
 int *array_range(int min, int max)
 {
@@ -15,10 +15,8 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	
 	l = max - min + 1;
 	a = malloc(sizeof(int) * l);
-
 	if (a == NULL)
 		return (NULL);
 	for (i = 0; i < l; i++, min++)
