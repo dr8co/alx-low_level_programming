62c474191c81882bbb034877ddc28437a9ae0a4b
Ian Duncan <ianduncanodhiambo@gmail.com>
2022-10-07 00:05:30 +0300

    Updated Betty documentation style on 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,14 +6,15 @@
  * malloc_checkout - allocate memory 
  * 
  * @b: the size of the memory to be allocated
- * return: a pointer to the allocated memory (SUCCESS)
+ * Return: a pointer to the allocated memory (SUCCESS)
  * Or normal termination error (FAIL)
  */
 void *malloc_checked(unsigned int b)
 {
-    void *s;
-    s = malloc(b);
-    if (s == NULL)
-        exit(98);
-    return s;
+	void *s;
+
+	s = malloc(b);
+	if (s == NULL)
+		exit(98);
+	return (s);
 }
