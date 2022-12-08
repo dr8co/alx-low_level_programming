63efa098b2150478e0c050422aac0786ea0e5d40
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2022-12-08 22:44:32 +0300

    Add advanced tasks

---
+++ 0x17-doubly_linked_lists/palindrome.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+/**
+ * @brief checks if a number is a palindrome.
+ * @param num the number to check.
+ * @return 1 if num is a palindrome, 0 otherwise.
+ */
+bool isPalindrome(int num) {
+    std::string str = std::to_string(num);
+    std::string rev{str};
+    std::reverse(rev.begin(), rev.end());
+    return rev == str;
+}
+
+int main() {
+    std::vector<int> arr{0};
+
+    for (int i = 999; i >= 99; --i) {
+        for (int j = i; j >= 99; --j) {
+
+            if (isPalindrome(i * j))
+                arr.push_back(i * j);
+        }
+    }
+    std::cout << "Max palindrome: " << *std::max_element(arr.begin(), arr.end()) << std::endl;
+}
