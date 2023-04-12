b779fa5db7d039934166c0dfcf316389faa79285
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-04-12 12:00:35 +0300

    Bugfix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
 		for (i = 0; i < size; ++i)
 		{
 			tmp = array[i];
-			printf("Value checked array[%zu] = [%d]\n", i, tmp);
+			printf("Value checked array[%lu] = [%d]\n", i, tmp);
 			if (tmp == value)
 				return (i);
 		}
